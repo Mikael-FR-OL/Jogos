127f28db1e7f74d9abbc97d84845fe4e9f7c1396
Mikael Ferreira <112272249+Mikael-FR-OL@users.noreply.github.com>
2024-12-19 02:49:00 -0300

    Update Advinhacao.py

--- Advinhacao.py
+++ Advinhacao.py
@@ -1,6 +1,6 @@
 import random
 print("*********************************************************")
-print("         Bem vindo ao jogo de advinhação                 ")
+print("         Bem vindo ao jogo de adivinhação                 ")
 print("*********************************************************")
 def jogo():
     numero_secreto = random.randint(1, 100)
@@ -10,7 +10,7 @@ def jogo():
         print("Você digitou", chute_str)
         chute_str = int(chute_str)
         if (chute_str < 1 or chute_str > 100):
-            print("DEVE INFORMAR UM ACIMA DE 1 E A MENOS DE 100")
+            print("Jogo com uma regra, por gentileza informe um numero de 1 a 100.")
             continue
 
         acertou = chute_str == numero_secreto
@@ -18,15 +18,15 @@ def jogo():
         menor = chute_str < numero_secreto
 
         if(acertou):
-            print("Acertoou")
+            print("PARABÉNNNNNSSSS")
             break
         elif (maior):
-            print("O seu chute foi maior do que o esperado")
+            print(f"Tente novamente, o seu numero {chute_str} foi maior do que esperado")
 
         elif(menor):
-            print("O seu chute foi menor do que o esperado")
+            print(f"Tente novamente, o seu numero {chute_str} foi maior do que esperado")
 
         else:
-            print("Valor invalido")
+            print(f"Valor inserido invalido {numero_secreto}")
     print("fim de jogo")
 if __name__ == '__main__':jogo()
